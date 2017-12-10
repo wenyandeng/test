d29d9d87b0f2b46265f3ce43f2c1e781cfa8ec91
dam <dam@damdeMacBook-Pro.local>
2017-12-10 20:24:40 +0800

    add printf

--- udp_s.c
+++ udp_s.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
@@ -8,11 +9,37 @@
 #define LOCAL_PORT 8890
 #define LOCAL_IP   "127.0.0.1"
 #define udp_printf printf
-int process_msg(void);
+int process_msg(void)
 {
 	return 0;
 }
 
+void my_printf(const struct sockaddr* sock, const char* buf, unsigned int len)
+{
+	if (sock != NULL){
+		udp_printf("sock :\n");
+		struct sockaddr_in* sock_inet = (struct sockaddr_in *)sock;
+		udp_printf("sock addr:[%s:%d]\n", (sock_inet->sin_addr).s_addr?inet_ntoa(sock_inet->sin_addr):"0.0.0.0", 
+											ntohs(sock_inet->sin_port));
+	}	
+
+	unsigned int i = 0;
+	udp_printf("========================================================\n");
+	while (i <= len && (*buf != '\0')) {
+		udp_printf("%02x", *(buf + i));
+		i++;
+		if (0 == i % 8) {
+			// blank
+			udp_printf("  ");
+		}
+		if (0 == i % 16) {
+			//\r\n
+			udp_printf("\r\n");
+		}
+	}
+	udp_printf("========================================================\n");
+	return;
+}
 int main(int argc, char *argv[])
 {
 	struct sockaddr_in local_sock;
@@ -40,15 +67,18 @@ int main(int argc, char *argv[])
 	}
 
 	unsigned int sock_len = sizeof(remote_sock);
-	char recvbuff[1204] = {0};
-	int n = 0;
+	char recvbuff[1024] = {0};
+
 	for(;;) {
-		n = recvfrom(s_fd, recvbuff, sizeof(recvbuff),  0, (struct sockaddr *)&remote_sock, &sock_len);
-		write(1, recvbuff, sizeof(recvbuff));
-		sleep(10);
+		int	n = recvfrom(s_fd, recvbuff, sizeof(recvbuff),  0, (struct sockaddr *)&remote_sock, &sock_len);
+		if (n > 0){
+			write(1, recvbuff, n);
+			write(s_fd, recvbuff, n);
+			continue;
+		}
+		fprintf(stderr, "Nothing recived!!\n");
 	}
 
 	close(s_fd);
-
 	return 0;
 }
